a1d7dbe2fd16576a518766f35c7616bf8f9f5cfc
annaa <anna.aakerman@outlook.com>
2026-09-05 19:04:20 +0200

    Making it better and more functional

--- terraform/main/eks.tf
+++ terraform/main/eks.tf
@@ -27,10 +27,21 @@ module "eks" {
   }
 }
 
+# Allow node SG to reach cluster API (ingress on the cluster SG)
+resource "aws_security_group_rule" "node_to_cluster_api" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.eks.cluster_security_group_id
+  source_security_group_id = module.eks.node_security_group_id
+  description              = "Nodes to EKS API"
+}
+
 output "cluster_name" {
   value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
-}
+}   
